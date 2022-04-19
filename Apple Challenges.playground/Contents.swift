002f1e65902e1a221514efe5b5e0503e99cb16d3
Mitchell <mitchellbudge@gmail.com>
2022-04-18 19:23:09 -0700

    Add Apple coding challenges template

---
+++ Apple Challenges.playground/Contents.swift
@@ -0,0 +1,23 @@
+import UIKit
+
+let url = "https://www.educative.io/blog/apple-coding-interview-questions"
+
+/*
+ Arrays and graphs questions
+ Determine sum of three integers
+ The goal of this exercise is to determine if the sum of three integers is equal to the given value.
+
+ Problem statement: Given an array of integers and a value, determine if there are any three integers in the array whose sum equals the given value.
+
+ Consider this array and the target sums.
+ */
+
+
+/*
+ Merge overlapping intervals
+ The goal of this exercise is to merge all the overlapping intervals of a given list to produce a list that has only mutually exclusive intervals.
+
+ Problem statement: You have an array (list) of interval pairs as input where each interval has a start and end timestamp, sorted by starting timestamps. Merge the overlapping intervals and return a new output array.
+
+ Consider an input array below. Intervals (1, 5), (3, 7), (4, 6), (6, 8) are overlapping so they should be merged to one interval (1, 8). Similarly, intervals (10, 12) and (12, 15) are also overlapping and should be merged to (10, 15).
+ */
